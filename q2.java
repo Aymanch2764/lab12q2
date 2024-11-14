7ec6669cd832cb0e664277e496a48107e2f7cf4c
MinhaMuhaamad <minhaghulammuhammad@gmail.com>
2024-11-13 23:28:42 -0800

    Removed greetUser() method call to introduce a bug

--- q2.java
+++ q2.java
@@ -1,5 +1,11 @@
 package q2lab12;
 
 public class q2 {
-
+	public static void main(String[] args) {
+        System.out.println("Welcome to the application!");
+        // greetUser(); // Removed this line to introduce a bug
+    }
+	public static void greetUser() {
+        System.out.println("Hello, User!");
+    }
 }
